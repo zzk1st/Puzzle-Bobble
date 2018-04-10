fa37d39aa3df0ce221ea59fd50ad32439b20be24
liuyang <ly@123.com>
2018-04-10 16:38:03 +0800

    add DoTween
    add dotween package
    add intro scene

---
+++ Assets/BubbleShooterEasterBunny/_scripts/IntroManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class IntroManager : MonoBehaviour
+{
+	public Button btnStart;
+	public Button btnSetting;
+	public GameObject panelSetting;
+	public Button btnCloseSetting;
+
+	public static IntroManager Instance{ private set; get; }
+
+
+
+	void Awake ()
+	{
+		if (Instance == null) {
+			Instance = this;
+		} else if (Instance != this) {
+			Destroy (gameObject);
+		}
+	}
+
+	void Start ()
+	{
+		btnStart.onClick.AddListener (() => {
+			//TODO
+		});
+
+		btnSetting.onClick.AddListener (() => {
+			OpenSlideLayout ();
+		});
+
+		btnCloseSetting.onClick.AddListener (() => {
+			CloseSlideLayout ();
+		});
+	}
+
+	//	void Update ()
+	//	{
+	//
+	//	}
+
+	//-----------------------------------------------------------------
+
+	void OpenSlideLayout ()
+	{
+		panelSetting.GetComponentInChildren<DOTweenAnimation> ().DORestartAllById ("slide");
+	}
+
+	void CloseSlideLayout ()
+	{
+		foreach (var item in panelSetting.GetComponentsInChildren<DOTweenAnimation>()) {
+			item.DOPlayBackwards ();
+		}
+	}
+}
